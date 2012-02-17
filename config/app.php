59f4a68f290c0a849bbe01b9cbb638fb8bbd0b98
William DURAND <william.durand1@gmail.com>
2012-02-17 01:28:38 +0100

    Refactored application, moved to a better location, added missing methods

---
+++ config/app.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__.'/../silex.phar';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\PropelServiceProvider(), array(
+    'propel.path'           => __DIR__ . '/../vendor/propel/runtime/lib',
+    'propel.config_file'    => __DIR__ . '/../config/conf/Propilex-conf.php',
+));
+
+$app['autoloader']->registerNamespaces(array(
+    'Propilex'  => __DIR__  . '/../src',
+));
+
+// Parser that removes "root" on JSON objects
+$app['json_parser'] = new \Propilex\Parser\JsonParser();
+
+// Debug?
+$app['debug'] = true;
+
+return $app;

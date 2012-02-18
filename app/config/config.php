777af6bf27139355fe168e9c9754a01c214dab77
William DURAND <william.durand1@gmail.com>
2012-02-18 12:37:02 +0100

    Use Propel Service Provider

--- app/config/config.php
+++ app/config/config.php
@@ -4,13 +4,14 @@ require_once __DIR__.'/../../vendor/silex.phar';
 
 $app = new Silex\Application();
 
-$app->register(new Silex\Provider\PropelServiceProvider(), array(
-    'propel.path'           => __DIR__ . '/../../vendor/propel/propel1/runtime/lib',
-    'propel.config_file'    => __DIR__ . '/conf/Propilex-conf.php',
+$app['autoloader']->registerNamespaces(array(
+    'Propel\Silex'  => __DIR__ . '/../../vendor/propel/propel-service-provider/src',
+    'Propilex'      => __DIR__ . '/../../src',
 ));
 
-$app['autoloader']->registerNamespaces(array(
-    'Propilex'  => __DIR__  . '/../../src',
+$app->register(new Propel\Silex\PropelServiceProvider(), array(
+    'propel.path'           => __DIR__ . '/../../vendor/propel/propel1/runtime/lib',
+    'propel.config_file'    => __DIR__ . '/conf/Propilex-conf.php',
 ));
 
 // Parser that removes "root" on JSON objects

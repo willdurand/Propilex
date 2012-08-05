7f38a53f0e300b8374fb96099a70f1d31ef4d3b6
Nicolas Béhier-Dévigne <nbehierdevigne@gmail.com>
2012-08-05 17:43:48 +0200

    Update schema.xml
    Add Model : Activity, Food, Location, Meal, User and UserMeal

---
+++ src/Propilex/Model/UserPeer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Propilex\Model;
+
+use Propilex\Model\om\BaseUserPeer;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'user' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.Propilex.Model
+ */
+class UserPeer extends BaseUserPeer
+{
+}
---
+++ src/Propilex/Model/UsermealPeer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Propilex\Model;
+
+use Propilex\Model\om\BaseUsermealPeer;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'userMeal' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.Propilex.Model
+ */
+class UsermealPeer extends BaseUsermealPeer
+{
+}

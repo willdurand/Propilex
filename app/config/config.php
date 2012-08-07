06b49e476e74609ef9f8f232b4eb6b3170a0907b
Nicolas Béhier-Dévigne <nbehierdevigne@gmail.com>
2012-08-07 16:08:13 +0200

    Test for Assetic use
    Can't succeed to config for now

--- app/config/config.php
+++ app/config/config.php
@@ -11,6 +11,36 @@ $app->register(new Propel\Silex\PropelServiceProvider(), array(
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path'	=> __DIR__ . '/../../views',
 ));
+/*
+$app->register(new SilexExtension\AsseticExtension(), array(
+    'assetic.options' => array(
+        'debug' => $app['debug']
+    ), 
+    'assetic.filters' => $app->protect(function ( $fm ) use($app )
+    {
+        $fm->set('yui_css', new Assetic\Filter\Yui\CssCompressorFilter($app['assetic.filter.yui_compressor.path']));
+        $fm->set('yui_js', new Assetic\Filter\Yui\JsCompressorFilter($app['assetic.filter.yui_compressor.path']));
+    }), 
+    'assetic.assets' => $app->protect(function ( $am, $fm ) use($app )
+    {
+        $am->set('styles', new Assetic\Asset\AssetCache(new Assetic\Asset\GlobAsset($app['assetic.input.path_to_css'], 
+        // Yui compressor is disabled by default.
+        // If you need it, and you have installed it, uncomment the
+        // next line, and delete "array()"
+        // array($fm->get('yui_css'))
+        array()), new Assetic\Cache\FilesystemCache($app['assetic.path_to_cache'])));
+        $am->get('styles')->setTargetPath($app['assetic.output.path_to_css']);
+        
+        $am->set('scripts', new Assetic\Asset\AssetCache(new Assetic\Asset\GlobAsset($app['assetic.input.path_to_js'], 
+        // Yui compressor is disabled by default.
+        // If you need it, and you have installed it, uncomment the
+        // next line, and delete "array()"
+        // array($fm->get('yui_js'))
+        array()), new Assetic\Cache\FilesystemCache($app['assetic.path_to_cache'])));
+        $am->get('scripts')->setTargetPath($app['assetic.output.path_to_js']);
+    })
+));
+*/
 
 // Parser that removes "root" on JSON objects
 $app['json_parser'] = new Propilex\Parser\JsonParser();

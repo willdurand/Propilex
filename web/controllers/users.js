115af33c59bfafb3e722865d990ff4b0b91ba318
Nicolas Béhier-Dévigne <nbehierdevigne@gmail.com>
2012-08-07 23:13:31 +0200

    Enhance edit-in-place field
    display input and save button

--- web/controllers/users.js
+++ web/controllers/users.js
@@ -32,23 +32,24 @@ App.Controllers.Users = Backbone.Router.extend({
   },
   
   fetchUsers: function() {
-	App.users = new App.Collections.Users([
-	    {"Id":1,"LocationId":1,"Firstname":"Nicolas","Lastname":"B\u00e9hier","Email":"nbd@gmail.com","Affiliation":null,"Description":"Ma Description","Photo":null,"Answered":"en attente de validation","CreatedAt":{"date":"2012-08-06 15:30:08","timezone_type":3,"timezone":"Europe\/Paris"},"UpdatedAt":{"date":"2012-08-06 15:30:08","timezone_type":3,"timezone":"Europe\/Paris"}},
-	    {"Id":2,"LocationId":2,"Firstname":"Simon, V\u00e9ronique et Rose","Lastname":"Assani","Email":"simon@gmail.com","Affiliation":"Polytech Tours","Description":null,"Photo":null,"Answered":"absent","CreatedAt":{"date":"2012-08-07 07:11:40","timezone_type":3,"timezone":"Europe\/Paris"},"UpdatedAt":{"date":"2012-08-07 07:11:40","timezone_type":3,"timezone":"Europe\/Paris"}},
-	    {"Id":3,"LocationId":1,"Firstname":"Flavien et Guylaine","Lastname":"Lenourichel","Email":"flavien@gmail.com","Affiliation":"Erasmus, Lyc\u00e9e","Description":null,"Photo":null,"Answered":"pr\u00e9sent","CreatedAt":{"date":"2012-08-07 07:11:40","timezone_type":3,"timezone":"Europe\/Paris"},"UpdatedAt":{"date":"2012-08-07 07:11:40","timezone_type":3,"timezone":"Europe\/Paris"}}
-	]);
-	new App.Views.IndexUser({ collection: App.users });
-	// Problème de chargement différé : 
-	// showAction ne connaît pas encore la collection, 
-	// utiliser des événements pour afficher après avoir reçu les données
-	/*App.users.fetch({
-	  success: function() {
-	    new App.Views.IndexUser({ collection: this.users });
-	  },
-	  error: function() {
-	    new Error({ message: "Error loading users." });
-	  }
-	});*/
+  	App.users = new App.Collections.Users([
+  	    {"Id":1,"LocationId":1,"Firstname":"Nicolas","Lastname":"B\u00e9hier","Email":"nbd@gmail.com","Affiliation":null,"Description":"Ma Description","Answered":"en attente de validation","CreatedAt":{"date":"2012-08-06 15:30:08","timezone_type":3,"timezone":"Europe\/Paris"},"UpdatedAt":{"date":"2012-08-06 15:30:08","timezone_type":3,"timezone":"Europe\/Paris"}},
+  	    {"Id":2,"LocationId":2,"Firstname":"Simon, V\u00e9ronique et Rose","Lastname":"Assani","Email":"simon@gmail.com","Affiliation":"Polytech Tours","Description":null,"Answered":"absent","CreatedAt":{"date":"2012-08-07 07:11:40","timezone_type":3,"timezone":"Europe\/Paris"},"UpdatedAt":{"date":"2012-08-07 07:11:40","timezone_type":3,"timezone":"Europe\/Paris"}},
+  	    {"Id":3,"LocationId":1,"Firstname":"Flavien et Guylaine","Lastname":"Lenourichel","Email":"flavien@gmail.com","Affiliation":"Erasmus, Lyc\u00e9e","Description":null,"Answered":"pr\u00e9sent","CreatedAt":{"date":"2012-08-07 07:11:40","timezone_type":3,"timezone":"Europe\/Paris"},"UpdatedAt":{"date":"2012-08-07 07:11:40","timezone_type":3,"timezone":"Europe\/Paris"}},
+        {"Id":4,"LocationId":1,"Firstname":"Camille","Lastname":"Ratia","Email":"camille@gmail.com","Affiliation":"Articque","Description":null,"Answered":"pr\u00e9sent","CreatedAt":{"date":"2012-08-07 07:11:40","timezone_type":3,"timezone":"Europe\/Paris"},"UpdatedAt":{"date":"2012-08-07 07:11:40","timezone_type":3,"timezone":"Europe\/Paris"}}
+  	]);
+  	new App.Views.IndexUser({ collection: App.users });
+  	// Problème de chargement différé : 
+  	// showAction ne connaît pas encore la collection, 
+  	// utiliser des événements pour afficher après avoir reçu les données
+  	/*App.users.fetch({
+  	  success: function() {
+  	    new App.Views.IndexUser({ collection: this.users });
+  	  },
+  	  error: function() {
+  	    new Error({ message: "Error loading users." });
+  	  }
+  	});*/
   }
   
 });

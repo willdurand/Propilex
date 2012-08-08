b6c9b7fb9e6b3a075ddde4b5131f614de5babb2e
Nicolas Béhier-Dévigne <nbehierdevigne@gmail.com>
2012-08-08 09:29:33 +0200

    Delete propilex-document-entities based files

--- app/propilex.php
+++ app/propilex.php
@@ -31,17 +31,7 @@ $app->get('/', function() use ($app) {
     			->find()
     			->exportTo($app['json_parser']);
     return $app['twig']->render('index.html.twig', array('users' => $users));
-    //return new Response(file_get_contents(__DIR__ . '/../web/index.html'), 200);
 });
-
-/**
- * Register a REST controller to manage documents
- */
-/*
-$app->mount('/documents', new Propilex\Provider\RestController(
-    'document', '\Propilex\Model\Document', 'getUpdatedAt'
-));
-*/
 
 /**
  * Register a REST controller to manage documents
@@ -49,14 +39,5 @@ $app->mount('/documents', new Propilex\Provider\RestController(
 $app->mount('/users', new Propilex\Provider\RestController(
 	'user', '\Propilex\Model\User', 'getUpdatedAt'
 ));
-
-/**
- * Get all Users Informations
- */
-$app->get('/getUsers', function() use ($app) {
-    $query = new Propilex\Model\UserQuery();
-    
-    return $app->json($query->leftJoinUsermeal()->leftJoinLocation()->find()->exportTo($app['json_parser'] ) );
-});
 
 return $app;

4945dee667fc16436b41991de37bf870a3143413
Nicolas Béhier-Dévigne <nbehierdevigne@gmail.com>
2012-08-09 08:56:00 +0200

    Enable to use key or value for enum
    - Add method to transform ENUM key and value
    - Enable to update user

--- src/Propilex/Model/User.php
+++ src/Propilex/Model/User.php
@@ -3,6 +3,7 @@
 namespace Propilex\Model;
 
 use Propilex\Model\om\BaseUser;
+use \PropelException;
 
 
 /**
@@ -18,4 +19,50 @@ use Propilex\Model\om\BaseUser;
  */
 class User extends BaseUser
 {
+    /**
+     * Set the value of [answered] column.
+     *
+     * @param int $v new value
+     * @return User The current object (for fluent API support)
+     * @throws PropelException - if the value is not accepted by this enum.
+     */
+    public function setAnswered($v)
+    {
+        if (intval($v) == $v) {
+            $v = self::getAnsweredValue(intval($v) );
+        }
+        
+        return parent::setAnswered($v);
+    }
+    
+    /**
+     * Get index of string ENUM value
+     * @param string $v
+     * @throws PropelException - if the value is not accepted by this enum
+     * @return int
+     */
+    public static function getAnsweredIndex($v)
+    {
+        $valueSet = UserPeer::getValueSet(UserPeer::ANSWERED);
+        $key = array_search($v, $valueSet);
+        if ($key === false) {
+            throw new PropelException('Unknown stored enum key: ' . $v);
+        }
+        return $key;
+    }
+    
+    /**
+     * Get value of a int key ENUM
+     * @param int $v
+     * @throws PropelException - if the key is not accepted by this enum
+     * @return string
+     */
+    public static function getAnsweredValue($v)
+    {
+        $valueSet = UserPeer::getValueSet(UserPeer::ANSWERED);
+        if (!isset($valueSet[$v])) {
+            throw new PropelException('Unknown stored enum key: ' . $v);
+        }
+        return $valueSet[$v];
+    }
 }

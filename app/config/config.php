330613d8e94a6374db68a05ac562001d86063a52
Nicolas Béhier-Dévigne <nbehierdevigne@gmail.com>
2012-08-14 14:42:35 +0200

    Try to use Assetics => unsuccess
    - Add SilexExtension\Assetics service provider
    - Add yuicompressor executable
    - Update twig template to use it
    - back-return after unsuccess

--- app/config/config.php
+++ app/config/config.php
@@ -4,6 +4,9 @@ require_once __DIR__.'/../../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+// Debug?
+$app['debug'] = true;
+
 // @see http://silex.sensiolabs.org/doc/providers/security.html
 //$app->register(new Silex\Provider\SecurityServiceProvider() );
 
@@ -15,41 +18,39 @@ $app->register(new Propel\Silex\PropelServiceProvider(), array(
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path'	=> __DIR__ . '/../../views',
 ));
-/*
+
 $app->register(new SilexExtension\AsseticExtension(), array(
+    'assetic.class_path' => __DIR__.'/../../vendor/assetic/src',
+    'assetic.path_to_web' => __DIR__ . '/../../web/assets',
+	'assetic.filter.yui_compressor.path' => __DIR__ . '/../../bin/yuicompressor/build/yuicompressor-2.4.7.jar',
+	//'assetic.output.path_to_css' => __DIR__ . '/../../assets/styles.css',
+	//'assetic.input.path_to_css' => __DIR__ . '/../../web/css/*.css',
+	//'assetic.path_to_cache' => __DIR__ . '/../../assets/cache',
     'assetic.options' => array(
-        'debug' => $app['debug']
+        'debug' => $app['debug'],
+        //'twig_support' => true
     ), 
     'assetic.filters' => $app->protect(function ( $fm ) use($app )
     {
+        $fm->set('less', new Assetic\Filter\LessphpFilter());
         $fm->set('yui_css', new Assetic\Filter\Yui\CssCompressorFilter($app['assetic.filter.yui_compressor.path']));
-        $fm->set('yui_js', new Assetic\Filter\Yui\JsCompressorFilter($app['assetic.filter.yui_compressor.path']));
     }), 
-    'assetic.assets' => $app->protect(function ( $am, $fm ) use($app )
+    /*'assetic.assets' => $app->protect(function ( $am, $fm ) use($app )
     {
-        $am->set('styles', new Assetic\Asset\AssetCache(new Assetic\Asset\GlobAsset($app['assetic.input.path_to_css'], 
-        // Yui compressor is disabled by default.
-        // If you need it, and you have installed it, uncomment the
-        // next line, and delete "array()"
-        // array($fm->get('yui_css'))
-        array()), new Assetic\Cache\FilesystemCache($app['assetic.path_to_cache'])));
+        $am->set('styles', 
+        		new Assetic\Asset\AssetCache(
+        			new Assetic\Asset\GlobAsset(
+        				$app['assetic.input.path_to_css'], 
+        				array($fm->get('yui_css'))
+        			), 
+        			new Assetic\Cache\FilesystemCache($app['assetic.path_to_cache'])
+        		)
+        );
         $am->get('styles')->setTargetPath($app['assetic.output.path_to_css']);
-        
-        $am->set('scripts', new Assetic\Asset\AssetCache(new Assetic\Asset\GlobAsset($app['assetic.input.path_to_js'], 
-        // Yui compressor is disabled by default.
-        // If you need it, and you have installed it, uncomment the
-        // next line, and delete "array()"
-        // array($fm->get('yui_js'))
-        array()), new Assetic\Cache\FilesystemCache($app['assetic.path_to_cache'])));
-        $am->get('scripts')->setTargetPath($app['assetic.output.path_to_js']);
-    })
+    })*/
 ));
-*/
 
 // Parser that removes "root" on JSON objects
 $app['json_parser'] = new Propilex\Parser\JsonParser();
-
-// Debug?
-$app['debug'] = true;
 
 return $app;

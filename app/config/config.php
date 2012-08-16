9e04fbfea3ebac3bc2d71b702c90640ff1552d1a
U-NET\nicolas.behierdevign <nicolas.behierdevign@CZC0025LBX.net.lan>
2012-08-16 17:27:51 +0200

    Update Propilex for dev dependances,  new route controller

--- app/config/config.php
+++ app/config/config.php
@@ -5,6 +5,7 @@ require_once __DIR__.'/../../vendor/autoload.php';
 $app = new Silex\Application();
 $app->register(new Propel\Silex\PropelServiceProvider(), array(
     'propel.config_file'    => __DIR__ . '/conf/Propilex-conf.php',
+	'propel.model_path'     => __DIR__ . '/../../src/Propilex/Model',
 ));
 
 // Parser that removes "root" on JSON objects

a9c8237bc43fac8337c47150b9571157723016ed
William DURAND <william.durand1@gmail.com>
2012-12-03 21:23:02 +0100

    Add new tests

---
+++ tests/Propilex/Tests/DocumentRestControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Propilex\Tests;
+
+use Silex\WebTestCase;
+
+class DocumentRestControllerTest extends WebTestCase
+{
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../../../app/propilex.php';
+        $app['debug'] = true;
+        $app['exception_handler']->disable();
+
+        return $app;
+    }
+
+    public function testGetDocuments()
+    {
+        $client   = $this->createClient();
+        $crawler  = $client->request('GET', '/documents/');
+        $response = $client->getResponse();
+
+        $this->assertJsonResponse($response);
+
+        $data = json_decode($response->getContent(), true);
+        foreach ($data as $item) {
+            $this->assertValidDocument($item);
+        }
+    }
+
+    protected function assertJsonResponse($response, $statusCode = 200)
+    {
+        $this->assertEquals(
+            $statusCode, $response->getStatusCode(),
+            $response->getContent()
+        );
+        $this->assertTrue(
+            $response->headers->contains('Content-Type', 'application/json'),
+            $response->headers
+        );
+    }
+
+    protected function assertValidDocument($document)
+    {
+        $this->arrayHasKey('id', $document);
+        $this->arrayHasKey('title', $document);
+        $this->arrayHasKey('body', $document);
+        $this->arrayHasKey('created_at', $document);
+    }
+}

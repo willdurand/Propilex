28ed8f07c400fb8210c466b634f89adb1183465d
William DURAND <william.durand1@gmail.com>
2014-01-22 13:24:50 +0100

    Use stack-cors

--- app/stack.php
+++ app/stack.php
@@ -2,4 +2,7 @@
 
 return (new Stack\Builder())
     ->push('Negotiation\Stack\Negotiation', $app['format.negotiator'])
+    ->push('Asm89\Stack\Cors', [
+        'allowedOrigins' => [ '*' ],
+    ])
     ->resolve($app);
